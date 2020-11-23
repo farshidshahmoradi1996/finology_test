d3025247f164a9a55e3542ae1efc61b3721155eb
Farshid Shahmoradi <farshidshahmoradi1996@gmail.com>
2020-11-23 18:26:21 +0330

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Helloooo</div>;
+  return (
+    <div className="App">
+      <h1 className="bg-red-600">Helloooo</h1>
+    </div>
+  );
 }
 
 export default App;
